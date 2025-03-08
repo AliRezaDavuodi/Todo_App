c4de838d749a57b2a224c4f1964d5300975e1b12
AliReza <AliRezaDavuodi@outlook.com>
2025-03-08 23:22:38 +0330

    feat: define todo route and handle get method

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import app from './server'
+import { todo } from './routes/todo/todo';
+
 
 const PORT: string = process.env.PORT ?? '3000'
+
+
+app.all('/', (_req, _res, next) => {
+    console.log('Time: ', Date.now());
+    next()
+})
+
+app.use('/todo', todo)
+
 
 app.listen(PORT, function (): void {
     console.log("Server is running on port " + PORT);
---
+++ src/interfaces/ITodo.ts
@@ -0,0 +1,7 @@
+export interface ITodo {
+    id: number,
+    title: string,
+    description: string
+    created_at: Date,
+    user_id: number
+}
