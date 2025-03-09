ab1bacd5a759fbd897c2728b8563db4e62da47de
AliReza <AliRezaDavuodi@outlook.com>
2025-03-09 21:37:21 +0330

    update: update setError fn

--- src/helper/sendError.ts
+++ src/helper/sendError.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express';
 
-export const sendError = (res: Response, message: string, code: number = 500) => {
+export const sendError = (res: Response, message: string | object, code: number = 500) => {
     return res.status(code).json({
         message,
         status: 'error',
