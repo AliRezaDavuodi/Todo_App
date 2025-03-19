638b564b635105ab852ab99bf804de39209fc7ed
AliReza <AliRezaDavuodi@outlook.com>
2025-03-19 20:38:28 +0330

    chore: complete CRUD for todo

--- src/controllers/todo/todo.ts
+++ src/controllers/todo/todo.ts
@@ -1,99 +1,97 @@
-import { Request, Response, Router } from "express";
-import { createTodoDto } from "../../dtos/todo/createTodoDto";
-import { UpdateTodoDto } from "../../dtos/todo/updateTodoDto";
-import { FileRepository } from "../../helper/file.repository";
-import { sendError } from "../../helper/sendError";
-import { sendSuccess } from "../../helper/sendSuccess";
-import { validateDto } from "../../helper/validateDto";
-import { ITodo } from "../../interfaces/ITodo";
+import { Request, Response, Router } from 'express'
+import { CreateTodoDto } from '../../dtos/todo/createTodoDto'
+import { UpdateTodoDto } from '../../dtos/todo/updateTodoDto'
+import { FileRepository } from '../../helper/file.repository'
+import { sendError } from '../../helper/sendError'
+import { sendSuccess } from '../../helper/sendSuccess'
+import { validateDto } from '../../helper/validateDto'
+import { TodoService } from '../../service/todo.service'
+import { TodoEntity } from '../../entities/todo.entity'
 
 export const todo: Router = Router()
-
+const todoService = new TodoService()
 
 todo.get('/', async (_req: Request, res: Response) => {
-    try {
-        const todos: ITodo[] = await FileRepository.readFile()
-        sendSuccess(res, todos)
-    } catch (error) {
-        console.log(error);
-        sendError(res, 'failed to fetch todos', 500)
-    }
+  try {
+    const todos: TodoEntity[] = await todoService.findAll()
+    sendSuccess(res, todos)
+  } catch (error) {
+    console.log(error)
+    sendError(res, 'failed to fetch todos', 500)
+  }
 })
 
-todo.post('/', validateDto(createTodoDto), async (req: Request<{}, {}, createTodoDto>, res: Response) => {
-    try {
-        const todos: ITodo[] = await FileRepository.readFile()
-        const newTodo: ITodo = {
-            id: Date.now(),
-            title: req.body.title,
-            description: req.body.description ?? "",
-            user_id: req.body.user_id,
-            status: 1,
-            created_at: new Date()
-        }
-        const newTodoData: ITodo[] = [...todos, newTodo]
-        await FileRepository.writeFile(newTodoData)
-        sendSuccess(res, newTodo, 201)
-    } catch (error) {
-        console.log(error);
-        sendError(res, 'Failed to create todo', 500)
-
-    }
-})
-
-todo.patch('/:id', validateDto(UpdateTodoDto), async (req: Request<{ id: string }, {}, UpdateTodoDto>, res: Response) => {
-    try {
-        const id = Number(req.params.id)
-        const todos = await FileRepository.readFile()
-        const todoIndex = todos.findIndex(todo => todo.id === id)
-
-        if (todoIndex === -1) {
-            sendError(res, "There is No Todo With This ID.", 404)
-            return
-        }
-
-        const updatedTodo = {
-            ...todos[todoIndex],
-            title: req.body?.title ?? todos[todoIndex].title,
-            description: req.body?.description ?? todos[todoIndex].description
-        }
-
-        todos.splice(todoIndex, 1, updatedTodo)
-
-        await FileRepository.writeFile(todos)
-
-        sendSuccess(res, todos[todoIndex], 200)
-
-    } catch (error) {
-        console.log(error);
-        sendError(res, 'update failed', 500)
+todo.post('/', validateDto(CreateTodoDto), async (req: Request<{}, {}, CreateTodoDto>, res: Response) => {
+  try {
+    const newTodo = {
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status || 1,
     }
 
+    const todo = await todoService.create(newTodo)
+
+    sendSuccess(res, todo, 201)
+  } catch (error) {
+    console.log(error)
+    sendError(res, 'Failed to create todo', 500)
+  }
 })
+
+todo.patch(
+  '/:id',
+  validateDto(UpdateTodoDto),
+  async (req: Request<{ id: string }, {}, UpdateTodoDto>, res: Response) => {
+    try {
+      const id = Number(req.params.id)
+      const todo = await todoService.findById(id)
+
+      if (!todo) {
+        sendError(res, 'Todo did NOT Found.', 404)
+        return
+      }
+
+      const updateTodo: UpdateTodoDto = {
+        title: req.body?.title ?? todo.title,
+        description: req.body?.description ?? todo.description,
+        status: req.body.status ?? todo.status,
+      }
+
+      const updatedInfo = await todoService.update(id, updateTodo)
+
+      sendSuccess(res, updatedInfo, 200)
+    } catch (error) {
+      console.log(error)
+      sendError(res, 'update failed', 500)
+    }
+  },
+)
 
 todo.delete('/:id', async (req: Request<{ id: string }>, res: Response) => {
-    try {
-        const todos = await FileRepository.readFile()
-        const id = Number(req.params.id)
+  try {
+    const id = Number(req.params.id)
 
-        if (!id) {
-            sendError(res, 'Please Put Todo id in the url.', 400)
-            return
-        }
-
-        const todoIndex = todos.findIndex(todo => todo.id === id);
-
-        if (todoIndex === -1) {
-            sendError(res, 'Todo NOT found.', 404)
-            return
-        }
-
-        const updateTodos = todos.filter(todo => todo.id !== id)
-        await FileRepository.writeFile(updateTodos)
-
-        res.status(204).send()
-
-    } catch (error) {
-        sendError(res, 'failed to deleted Todo', 500)
+    if (!id) {
+      sendError(res, 'Please Put Todo id in the url.', 400)
+      return
     }
+
+    const todo = await todoService.findById(id)
+
+    if (!todo) {
+      sendError(res, 'Todo Not Found.', 404)
+      return
+    }
+
+    const todoId = await todoService.delete(id)
+
+    const deleteData = {
+      delete: true,
+      id: todoId,
+    }
+
+    sendSuccess(res, deleteData, 200)
+  } catch (error) {
+    sendError(res, 'failed to deleted Todo', 500)
+  }
 })
